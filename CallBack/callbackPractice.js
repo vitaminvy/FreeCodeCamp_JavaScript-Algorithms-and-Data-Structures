4576761d3ea622dd9032956a0731e039627ffb25
titvvn <hovuongtuongvy2711@gmail.com>
2025-09-20 03:05:54 +0700

    add 129ex: every

--- CallBack/callbackPractice.js
+++ CallBack/callbackPractice.js
@@ -51,18 +51,38 @@ console.log(numbers.mySome(function (number, index, array) {
     return array.length % 2 === 0;
 }));
 
+//129 every
+Array.prototype.myEvery = function(cb) {
+    for(var index in this){
+        if(this.hasOwnProperty(index)){
+            if(!cb(this[index],index,this))return false;
+        }
+    }
+    return true;
+    
+}
+const numb = [1, 3, 3, 5];
+console.log(numbers.myEvery(function (number) {
+  return number % 2 !== 0;}))
 
-/**
-Expected results:
 
-const numbers = [1, 3, 3, 5];
+// Expected results
+
+// const numbers = [1, 3, 3, 5];
+
+// console.log(numbers.myEvery(function (number) {
+//     return number % 2 !== 0;
+// })); // Output: true
+
+// console.log(numbers.myEvery(function (number, index) {
+//     return index % 2 === 0;
+// })); // Output: false
+
+// console.log(numbers.myEvery(function (number, index, array) {
+//     return array.length % 2 === 0;
+// })); // Output: true
 
 
-
- Output: true
-
- Output: true
- */
 
 
 
