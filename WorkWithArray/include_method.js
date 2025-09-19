f15e9e98cd6efca420a30c252ebba9ecd0db2978
titvvn <hovuongtuongvy2711@gmail.com>
2025-09-19 14:02:07 +0700

    include method for string and array

---
+++ WorkWithArray/include_method.js
@@ -0,0 +1,8 @@
+// include có sẵn trong array/string
+
+var title = "Responsive web desgin";
+console.log(title.includes('web desgin'),1); // output: true
+
+var course = ["javascript", "PHP", "Ruby"]
+// 3 + - 1 = 2
+console.log(course.includes('Ruby', -1));
