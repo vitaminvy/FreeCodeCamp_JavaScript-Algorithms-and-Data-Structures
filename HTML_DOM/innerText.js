ace492067c3b10c14efec60509516f0b59dc937a
titvvn <hovuongtuongvy2711@gmail.com>
2025-09-22 14:26:33 +0700

    innerText , textContent

---
+++ HTML_DOM/innerText.js
@@ -0,0 +1,10 @@
+//innerText,textContent
+//textContent: lấy ra tất cả, bao gồm nội dung con, kể cả code
+//innerText: chỉ lấy nội dung giống như trên trình duyệt
+// innerText  là thuộc tính của element node
+//textContent: thuộc tính của element node, text node
+var headingElement = 
+    document.querySelector('h1');
+console.log(headingElement.textContent)
+
+
